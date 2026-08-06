91cc3b25fee9b4647136d4cadc249a97ae528060
AngelGonePro <97822157+AngelGonePro@users.noreply.github.com>
2026-08-06 05:27:38 -0600

    Update preload.js
    fix fps issues

--- preload.js
+++ preload.js
@@ -11,8 +11,10 @@ contextBridge.exposeInMainWorld('electronAPI', {
   calibrateNpl: (payload) => ipcRenderer.invoke('calibrate-npl', payload),
   runFFmpegWithProgress: (payload) => ipcRenderer.invoke('run-ffmpeg-with-progress', payload),
   runPipedEncode: (payload) => ipcRenderer.invoke('run-piped-encode', payload),
+  runTriplePipedEncode: (payload) => ipcRenderer.invoke('run-triple-piped-encode', payload),
   killFFmpegJob: (payload) => ipcRenderer.invoke('kill-ffmpeg-job', payload),
   deleteTempFile: (payload) => ipcRenderer.invoke('delete-temp-file', payload),
+  writeTextFile: (payload) => ipcRenderer.invoke('write-text-file', payload),
   extractAndScaleChapters: (payload) => ipcRenderer.invoke('extract-and-scale-chapters', payload),
   onFFmpegProgress: (callback) => ipcRenderer.on('ffmpeg-progress', (event, data) => callback(data))
 });
